915fc0e40324ad15fc9c8f8db35a65a8fedab633
Soumen Shit <soumenshit907@gmail.com>
2025-08-14 14:38:06 +0530

    add redis public link

--- src/redisClient.js
+++ src/redisClient.js
@@ -2,7 +2,7 @@ const Redis = require("ioredis");
 const dotenv = require("dotenv");
 dotenv.config();
 
-const redis = new Redis(process.env.REDIS_URL || "redis://localhost:6379");
+const redis = new Redis(process.env.REDIS_URL);
 
 redis.on("connect", () => console.log("Redis connected"));
 redis.on("error", (err) => console.error("Redis error", err));
